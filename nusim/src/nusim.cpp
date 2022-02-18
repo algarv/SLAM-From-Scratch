1e6e6be73f8074a5392cf4a8cf2aa72b0581cbaa
Anna Garverick <annagarverick2022@u.northwestern.edu>
2022-02-17 23:45:52 -0600

    Adding noise to nusim

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -37,24 +37,35 @@
 #include "turtlelib/rigid2d.hpp"
 #include "turtlelib/diff_drive.hpp"
 #include <ros/console.h>
+#include <random>
 
 static int rate;
 static bool teleporting = false;
-static double x = 0, y = 0, w = 0, x_length = 0, y_length = 0, width = 0, left_rot_vel = 0.0, right_rot_vel = 0.0, mticks_radsec, eticks_rad;
+static double x = 0, y = 0, w = 0, x_length = 0, y_length = 0, width = 0, left_rot_vel = 0.0, right_rot_vel = 0.0, mticks_radsec, eticks_rad, max_range, slip_min, slip_max, ada_L, ada_R;
 static std::string left_wheel = "red_wheel_left_joint", right_wheel = "red_wheel_right_joint", odom_id;
 std::vector<double> obj_x_list, obj_y_list, obj_d_list, radsec;
 static std_msgs::UInt64 ts;
 static sensor_msgs::JointState wheels;
 static geometry_msgs::TransformStamped sim_tf;
 static nuturtlebot_msgs::SensorData sensor_data; 
-static ros::Publisher obj_pub, js_pub, ts_pub, arena_pub, sensor_pub;
+static ros::Publisher obj_pub, js_pub, ts_pub, arena_pub, sensor_pub, fake_sensor_pub;
 static ros::Subscriber wheel_sub;
 static ros::ServiceServer rs_service, tp_service;
-static visualization_msgs::MarkerArray obstacle, obj_array, marker_arena, arena_array;
-static turtlelib::Wheel_Angle wheel_angles, old_wheel_angles;
+static visualization_msgs::MarkerArray obstacle, obj_array, marker_arena, arena_array, fake_sensor_array;
+static turtlelib::Wheel_Angle theoretical_wheel_angles, wheel_angles, old_wheel_angles;
 static turtlelib::Wheel_Angular_Velocities wheel_vels;
 static turtlelib::Twist2D twist;
 static turtlelib::q pos, old_pos;
+
+ std::mt19937 & get_random()
+ {
+     // static variables inside a function are created once and persist for the remainder of the program
+     static std::random_device rd{}; 
+     static std::mt19937 mt{rd()};
+     // we return a reference to the pseudo-random number genrator object. This is always the
+     // same object every time get_random is called
+     return mt;
+ }
 
 bool restart(std_srvs::Empty::Request&, std_srvs::Empty::Response&){
 /// \brief Send the turtle bot back to the origin of the world frame and restart the timestep counter.
@@ -229,21 +240,102 @@ visualization_msgs::MarkerArray make_arena(float x_length, float y_length){
 
 }
 
+visualization_msgs::MarkerArray fake_sensor(turtlelib::q robot_pos, std::vector<double> obj_x_list, std::vector<double> obj_y_list){
+
+    int id = 0;
+
+    fake_sensor_array.markers.resize(obj_x_list.size());
+
+    std::normal_distribution<> noise(0, .01);
+
+    for (unsigned int i = 0; i<obj_x_list.size(); i+=1) {
+        
+        double noise_x = noise(get_random());
+        double noise_y = noise(get_random());
+
+        turtlelib::Vector2D trans_wr;
+        trans_wr.x = robot_pos.x;
+        trans_wr.y = robot_pos.y;
+
+        turtlelib::Transform2D T_wr(trans_wr, robot_pos.theta);
+        turtlelib::Transform2D T_rw(0);
+        T_rw = T_wr.inv();
+
+        turtlelib::Vector2D obstacle_world;
+        obstacle_world.x = obj_x_list[i];
+        obstacle_world.y = obj_y_list[i];
+
+        turtlelib::Vector2D obstacle_robot = T_rw(obstacle_world);
+        ros::Duration duration(1.0);
+
+        double d = sqrt(pow(robot_pos.x-obj_x_list[i],2) + pow(robot_pos.y-obj_y_list[i],2));
+        ROS_WARN("Distance %d: %6.2f",i,d);
+        if (d > max_range) {
+            fake_sensor_array.markers[i].action = visualization_msgs::Marker::DELETE;
+            ROS_WARN("Deleting %d",i);
+        }
+        else {
+            fake_sensor_array.markers[i].action = visualization_msgs::Marker::ADD;
+        }
+
+        fake_sensor_array.markers[i].header.frame_id = "red_base_footprint";
+        fake_sensor_array.markers[i].ns = "nusim_node";
+        fake_sensor_array.markers[i].header.stamp = ros::Time::now();
+        fake_sensor_array.markers[i].type = visualization_msgs::Marker::CYLINDER;
+        fake_sensor_array.markers[i].id = id;
+        fake_sensor_array.markers[i].pose.position.x = obstacle_robot.x + noise_x;
+        fake_sensor_array.markers[i].pose.position.y = obstacle_robot.y + noise_y;
+        fake_sensor_array.markers[i].pose.position.z = .125;
+        fake_sensor_array.markers[i].pose.orientation.x = 0.0;
+        fake_sensor_array.markers[i].pose.orientation.y = 0.0;
+        fake_sensor_array.markers[i].pose.orientation.z = 0.0;
+        fake_sensor_array.markers[i].pose.orientation.w = 1.0;
+        fake_sensor_array.markers[i].scale.x = obj_d_list[i];
+        fake_sensor_array.markers[i].scale.y = obj_d_list[i];
+        fake_sensor_array.markers[i].scale.z = .25;
+        fake_sensor_array.markers[i].color.a = 1.0;
+        fake_sensor_array.markers[i].color.r = 0.0;
+        fake_sensor_array.markers[i].color.g = 1.0;
+        fake_sensor_array.markers[i].color.b = 0.0;
+        fake_sensor_array.markers[i].lifetime = duration;
+        fake_sensor_array.markers[i].frame_locked = true;
+        id += 1;
+    }
+
+    return fake_sensor_array;
+}
+
 void update_wheel_position(const nuturtlebot_msgs::WheelCommands::ConstPtr &wheel_cmd){
 
     double L_cmd = wheel_cmd->left_velocity;
     double R_cmd = wheel_cmd->right_velocity;
 
-    // left_rot_vel = 2.84 * L_cmd / 265.0;
-    // right_rot_vel = 2.84 * R_cmd / 265.0;
     left_rot_vel = L_cmd * mticks_radsec;
     right_rot_vel = R_cmd * mticks_radsec;
+    
+    double mean_L = left_rot_vel;
+    double mean_R = right_rot_vel;
+    double std = .1;
 
-    wheel_vels.L = left_rot_vel;
-    wheel_vels.R = right_rot_vel;
+    std::normal_distribution<> L(mean_L, std);
+    std::normal_distribution<> R(mean_R, std);
 
-    wheel_angles.L = (left_rot_vel / rate) + old_wheel_angles.L;
-    wheel_angles.R = (right_rot_vel / rate) + old_wheel_angles.R;
+    wheel_vels.L = L(get_random());
+    wheel_vels.R = R(get_random());
+
+    // ROS_WARN("Without Noise: %6.2f, With Noise: %6.2f \n",L_cmd * mticks_radsec, wheel_vels.L);
+    std::uniform_real_distribution dist(slip_min, slip_max);
+
+    ada_L = dist(get_random());
+    ada_R = dist(get_random());
+
+    // ROS_WARN("L: %6.5f, R: %6.5f", slip_min, slip_max);
+
+    theoretical_wheel_angles.L = ((left_rot_vel / rate) + old_wheel_angles.L);
+    theoretical_wheel_angles.R = ((right_rot_vel / rate) + old_wheel_angles.R);
+
+    wheel_angles.L = (ada_L * wheel_vels.L) + theoretical_wheel_angles.L;
+    wheel_angles.R = (ada_R * wheel_vels.R) + theoretical_wheel_angles.R;
 
     sensor_data.stamp = ros::Time::now();
     sensor_data.left_encoder = wheel_angles.L / eticks_rad;
@@ -272,6 +364,7 @@ int main(int argc, char *argv[]){
     obj_pub = nh.advertise<visualization_msgs::MarkerArray>("visualization_marker_array", rate);
     arena_pub = nh.advertise<visualization_msgs::MarkerArray>("visualization_marker_array", rate);
     sensor_pub = pub_nh.advertise<nuturtlebot_msgs::SensorData>("sensor_data", rate);
+    fake_sensor_pub = nh.advertise<visualization_msgs::MarkerArray>("/fake_sensor", 5);
 
     wheel_sub = pub_nh.subscribe("red/wheel_cmd", 10, update_wheel_position);
 
@@ -286,7 +379,10 @@ int main(int argc, char *argv[]){
     nh.getParam("wall_width", width);
     nh.getParam("/motor_cmd_to_radsec", mticks_radsec);
     nh.getParam("/encoder_ticks_to_rad", eticks_rad);
-    
+    nh.getParam("/max_range",max_range);
+    nh.getParam("/slip_min",slip_min);
+    nh.getParam("/slip_max",slip_max);
+
     nh.param<std::string>("odom_id",odom_id,"odom");
 
     radsec.resize(2);
@@ -325,8 +421,8 @@ int main(int argc, char *argv[]){
 
         // js_pub.publish(wheels);
 
-        if (teleporting == false){
-            pos = DD.get_q(wheel_angles, old_wheel_angles, old_pos);
+        if (teleporting == false){            
+            pos = DD.get_q(theoretical_wheel_angles, old_wheel_angles, old_pos);
             // pos = DD.get_q(twist,old_pos);
         }
         
@@ -345,11 +441,14 @@ int main(int argc, char *argv[]){
 
         broadcaster.sendTransform(sim_tf);
         
+        fake_sensor_array = fake_sensor(pos, obj_x_list, obj_y_list);
+        fake_sensor_pub.publish(fake_sensor_array);
+
         nh.getParam("/obj_x",obj_x_list);
         nh.getParam("/obj_y",obj_y_list);
         nh.getParam("/obj_d",obj_d_list);
 
-        old_wheel_angles = wheel_angles;
+        old_wheel_angles = theoretical_wheel_angles;
         old_pos = pos; 
 
         teleporting = false;
